8ced33a484893e3483d839491a5bb1dd5e84ac34
Lim Zheng Yang <zhengyanglim057@gmail.com>
2022-04-30 18:12:04 +0800

    simple bioinformatics dna count

--- dna_nycleotide_count_web_app.py
+++ dna_nycleotide_count_web_app.py
@@ -21,7 +21,7 @@ This app counts the nucleotide composition of query DNA!
 #######################################################
 # st.sidebar.header('Enter DNA sequence')
 st.header('Enter DNA sequence')
-sequence_input = "DNA Query \ntest1 for sequence input \nstart of the second line \n3rd line test"
+sequence_input = "DNA Query \nTJNKJGNKJCNKJANCKJNKGJNKANCKJNKJTNKJNGKJNC\nLKEDMFLKEMGLKMTKMCKMKGMLKAMKMGKMKGMKCMKTMKMA \nGGGKGMKGMGKMGKMGKGMKMCKAAAAAAMKCMKMKMKMKMAKMHBRHUHTUH"
 
 # sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
 sequence = st.text_area("Sequence input", sequence_input, height=150)
@@ -32,8 +32,63 @@ sequence
 sequence = sequence[1:]
 sequence
 # concatenate list to string
-sequence = ''.join(sequence)
+sequence = ' * '.join(sequence)
 
 st.write("""
 ***
 """)
+
+# prints the input DNA sequence
+st.header('INPUT (DNA Query)')
+sequence
+
+# DNA nucleotide count
+st.header('OUTPUT (DNA Nucleotide Count)')
+
+# 1. print dictionary
+st.subheader('1. Print dictionary')
+def DNA_nucleotide_count(seq):
+    d = dict([
+        ('A',seq.count('A')),
+        ('T',seq.count('T')),
+        ('G',seq.count('G')),
+        ('C',seq.count('C'))
+    ])
+    return d
+
+X = DNA_nucleotide_count(sequence)
+
+X_label = list(X)
+X_values = list(X.values())
+
+X
+
+# 2. print text
+st.subheader("2. Print text")
+st.write("There are " + str(X['A']) + ' adenine (A)')
+st.write("There are " + str(X['T']) + ' thymine (T)')
+st.write("There are " + str(X['G']) + ' adeline (guanine) (G)')
+st.write("There are " + str(X['C']) + ' thymine (cytosine) (C)')
+
+# 3. display dataframe
+st.subheader("3. Display Dataframe")
+df = pd.DataFrame.from_dict(X, orient="index")
+df = df.rename({0: "count"}, axis="columns")
+df.reset_index(inplace=True)
+df = df.rename(columns = {"index": "nucleotide"})
+st.write(df)
+
+# 4. display bar chart using altair
+st.subheader("4. Display Bar Chart")
+p = alt.Chart(df).mark_bar().encode(
+    x="nucleotide",
+    y="count"
+)
+
+# widen the bar chart
+p = p.properties(
+    width=alt.Step(50)
+)
+
+# display
+st.write(p)
